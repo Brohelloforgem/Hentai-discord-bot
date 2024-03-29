443783aea7995dc7fd2f6c56c9bab39a5025b8f8
Brohelloforgem <146630422+Brohelloforgem@users.noreply.github.com>
2024-03-29 22:49:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-token = ""
+token = os.getenv(Token)
 # the id of the chat to send bot logs of admin value i.e
 # (kick, ban, purge, etc)
-admin_actions_log_channel_id = 741923499542839330
+admin_actions_log_channel_id = 1196710107493433395
 # the id of the chat to send bot logs of like general commands that were
 # executed
-general_actions_log_channel_id = 741923499542839330
+general_actions_log_channel_id = 1196710107493433395
 # the name of the role the user should get once theyve been verified
-welcome_message_channel_name = "general"
+welcome_message_channel_name = ""
 verify_role_name = 'Verified'
 # the prefix to be used
 conf_bot_prefix = "!"
